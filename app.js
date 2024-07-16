b189518bac57940fef6a901fdc2c85d54556c24b
Ansuofficial <badjieansu165@gmail.com>
2024-07-16 13:26:04 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cartData = require("./pricing-card-data");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json(cartData);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
---
+++ pricing-card-data.js
@@ -0,0 +1,54 @@
+module.exports  = [
+  {
+    pricingPlans: [
+      {
+        name: "Basic Plan",
+        price: "9.99",
+        currency: "USD",
+        billingCycle: "monthly",
+        features: [
+          "Access to core features",
+          "10 GB storage",
+          "Email support",
+          "Basic analytics",
+          "Community access",
+        ],
+        description:
+          "Perfect for individuals or small teams starting out and needing essential features at an affordable price.",
+      },
+      {
+        name: "Standard Plan",
+        price: "29.99",
+        currency: "USD",
+        billingCycle: "monthly",
+        features: [
+          "Access to all Basic features",
+          "100 GB storage",
+          "Priority email support",
+          "Advanced analytics",
+          "Integration with third-party apps",
+          "Monthly webinars and tutorials",
+        ],
+        description:
+          "Ideal for growing teams and businesses that need more storage and advanced features to scale their operations efficiently.",
+      },
+      {
+        name: "Premium Plan",
+        price: "59.99",
+        currency: "USD",
+        billingCycle: "monthly",
+        features: [
+          "Access to all Standard features",
+          "1 TB storage",
+          "24/7 phone and email support",
+          "Customizable dashboard",
+          "Enhanced security features",
+          "Dedicated account manager",
+          "Quarterly business reviews",
+        ],
+        description:
+          "Best suited for larger organizations or businesses requiring top-tier support, extensive storage, and customized solutions to drive growth and performance.",
+      },
+    ],
+  }
+];
